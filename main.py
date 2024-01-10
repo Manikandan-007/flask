651784e4b3f22a004d5d4fba37e81deb39a48a4c
Manikandan-007 <98259460+Manikandan-007@users.noreply.github.com>
2024-01-10 21:42:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -386,4 +386,6 @@ def index():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    socketio.run(app, debug=True)
+    # socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+

8772393557e27f5e8eaab8f4f243017671ee016f
Manikandan-007 <98259460+Manikandan-007@users.noreply.github.com>
2024-01-10 21:49:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -384,8 +384,10 @@ def infoGsm():
 def index():
     return jsonify({'Hi':'Hi'})
 
-if __name__ == '__main__':
-    # app.run(debug=True)
-    # socketio.run(app, debug=True)
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     # app.run(debug=True)
+#     # socketio.run(app, debug=True)
+#     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
 
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
